c92279f2d8bf656483a60bb39addf9c18913c03c
Дмитрий Беленикин <info@agency-web.ru>
2026-02-17 15:26:56 +0300

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,19 @@ const isMobile = {
     },
     BlackBerry: function () {
         return navigator.userAgent.match(/BlackBerry/i);
-        
+
     },
     iOS: function () {
         return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        
+
     },
     Opera: function () {
         return navigator.userAgent.match(/Opera Mini/i);
-        
+
     },
     Windows: function () {
         return navigator.userAgent.match(/IEMobile/i);
-        
+
     },
     any: function () {
         return (
@@ -26,22 +26,12 @@ const isMobile = {
             isMobile.BlackBerry() ||
             isMobile.iOS() ||
             isMobile.Opera() ||
-            isMobile.Windows());  
+            isMobile.Windows());
     }
 };
 
 if (isMobile.any()) {
     document.body.classList.add('_touch');
-
-    let menuArrows = document.querySelectorAll('.drop-down-list, .menu__arrow');
-    if (menuArrows.length > 0) {
-        for (let index = 0; index < menuArrows.length; index++) {
-            const menuArrow = menuArrows[index];
-            menuArrow.addEventListener("click", function (e) {
-                menuArrow.parentElement.classList.toggle('_active');
-            });
-        }
-    }
 
 } else {
     document.body.classList.add('_pc');
